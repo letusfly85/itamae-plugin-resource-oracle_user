9fbea5eb210cde51a77ecae05c1ab0908aabcec3
letusfly85 <letusfly95@gmail.com>
2015-05-06 23:57:21 +0900

    first commit

---
+++ lib/itamae/plugin/resource/oracle_user.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/oracle_user/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module OracleUser
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/itamae/plugin/resource/oracle_user_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Itamae::Plugin::Resource::OracleUser do
+  it 'has a version number' do
+    expect(Itamae::Plugin::Resource::OracleUser::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
